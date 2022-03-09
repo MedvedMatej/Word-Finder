83c9185dbc7b8c1a0018a45e5dd4069b2fdc1bac
kyousei-dev <matej.medved01@gmail.com>
2022-03-09 19:40:43 +0100

    add excluded chars

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 A simple tool to help with word puzzles from Slovenian language.
 
 # How to use
-python word-finder.py param1 param2\
+python word-finder.py param1 param2 param3\
 param1 - characters that the word must include (E.g. abc)\
-param2 (optional) - position of known characters (E.g. 1a2b3c)
+param2 - characters that are not in the word
+param3 - (optional) - position of known characters (E.g. 1a2b3c)
--- word-finder.py
+++ word-finder.py
@@ -6,12 +6,14 @@ if __name__ == "__main__":
     else:
         included_chars = sys.argv[1]
         known_chars = None
-        if(len(sys.argv) >= 3):
-            known_chars = sys.argv[2]
+        forbidden_chars = sys.argv[2]
+        if(len(sys.argv) >= 4):
+            known_chars = sys.argv[3]
             known_chars = [known_chars[i:i+2] for i in range(0,len(known_chars),2)]
         with open("normalized-words.txt", "r", encoding="utf-8") as f:
             words = f.read().splitlines()
             filtered = [word for word in words if 0 not in [c in word for c in included_chars]]
+            filtered = [word for word in filtered if 1 not in [c in word for c in forbidden_chars]]
             if known_chars:
                 filtered = [word for word in filtered if 0 not in [kchar[1]== word[int(kchar[0])-1] for kchar in known_chars]]
             print(filtered)
