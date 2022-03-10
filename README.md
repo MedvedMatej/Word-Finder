abeefd6aa648ecf56f2514c8d1d53780b5bb2b72
Matej Medved <matej.medved01@gmail.com>
2022-03-10 11:52:22 +0100

    Add forbidden character locations as 4th paramater

--- README.md
+++ README.md
@@ -5,4 +5,7 @@ A simple tool to help with word puzzles from Slovenian language.
 python word-finder.py param1 param2 param3\
 param1 - characters that the word must include (E.g. abc)\
 param2 - characters that are not in the word\
-param3 - (optional) - position of known characters (E.g. 1a2b3c)
+param3 - position of known characters (E.g. 1a2b3c)\
+param4 - position in which character must not be (E.g. 1c2a3b)
+
+If you wish to skip a parameter you can put "-" in it's place.
--- word-finder.py
+++ word-finder.py
@@ -1,21 +1,43 @@
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
+    if len(sys.argv) < 3:
         print("Invalid number of arguments.")
     else:
-        included_chars = sys.argv[1]
+        included_chars = None
+        forbidden_chars = None
         known_chars = None
-        forbidden_chars = sys.argv[2]
-        if(len(sys.argv) >= 4):
+        forbidden_char_loc = None
+
+        if len(sys.argv) >=2 and sys.argv[1] != "-":
+            included_chars = sys.argv[1]
+
+        if len(sys.argv) >= 3 and sys.argv[2] != "-":
+            forbidden_chars = sys.argv[2]
+
+        if len(sys.argv) >= 4 and sys.argv[3] != "-":
             known_chars = sys.argv[3]
             known_chars = [known_chars[i:i+2] for i in range(0,len(known_chars),2)]
+
+        if len(sys.argv) >= 5 and sys.argv[4] != "-":
+            forbidden_char_loc = sys.argv[4]
+            forbidden_char_loc = [forbidden_char_loc[i:i+2] for i in range(0,len(forbidden_char_loc),2)]
+        
         with open("normalized-words.txt", "r", encoding="utf-8") as f:
-            words = f.read().splitlines()
-            filtered = [word for word in words if 0 not in [c in word for c in included_chars]]
-            filtered = [word for word in filtered if 1 not in [c in word for c in forbidden_chars]]
+            filtered = f.read().splitlines()
+            
+            if included_chars:
+                filtered = [word for word in filtered if 0 not in [c in word for c in included_chars]]
+
+            if forbidden_chars:
+                filtered = [word for word in filtered if 1 not in [c in word for c in forbidden_chars]]
+
             if known_chars:
                 filtered = [word for word in filtered if 0 not in [kchar[1]== word[int(kchar[0])-1] for kchar in known_chars]]
-            print(filtered)
+            
+            if forbidden_char_loc:
+                filtered = [word for word in filtered if 1 not in [forbidden[1] == word[int(forbidden[0])-1] for forbidden in forbidden_char_loc]]
+
+            print(set(filtered))
 
     
